37b8d282c59dbf75406fd13e107ab0cf3eb3da34
LucasButto <lbutto40@gmail.com>
2022-04-04 19:59:30 -0300

    ejercicio 6 resuelto

--- exercises/exercise6.py
+++ exercises/exercise6.py
@@ -6,7 +6,14 @@ Restricción: Utilizar el método append
 """
 
 # COMPLETAR - INICIO
-lista_01 = 
+
+lista_01 = []
+
+lista_01.append(1)
+lista_01.append(2)
+lista_01.append(3)
+lista_01.append(4)
+
 # COMPLETAR - FIN
 
 assert len(lista_01) == 4
@@ -20,6 +27,8 @@ Restricción: Utilizar el método pop
 lista = ["ho", "la", 81, 6, 42, "como", "estas?"]
 
 # COMPLETAR - INICIO
+
+elemento_extraido=lista.pop(3)
 
 # COMPLETAR - FIN
 
@@ -37,6 +46,12 @@ lista_c = ["siete", "ocho", "nueve"]
 
 # COMPLETAR - INICIO
 
+listas_concatenadas_01=[]
+
+listas_concatenadas_01.extend(lista_a)
+listas_concatenadas_01.extend(lista_b)
+listas_concatenadas_01.extend(lista_c)
+
 # COMPLETAR - FIN
 
 assert listas_concatenadas_01 == [1, 2, 3, "4", "5", "6", "siete", "ocho", "nueve"]
@@ -52,6 +67,8 @@ lista_nueva = [0, 1, 3, 4]
 
 # COMPLETAR - INICIO
 
+lista_nueva.insert(2,variable_01)
+
 # COMPLETAR - FIN
 
 assert lista_nueva == [0, 1, 2, 3, 4]
@@ -65,6 +82,10 @@ Restricción: Utilizar el método append junto al indexado simple
 lista = ["ho", 3.1416, 42, 81, 6, "la"]
 
 # COMPLETAR - INICIO
+lista_primero_y_ultimo=[]
+
+lista_primero_y_ultimo.append(lista[0])
+lista_primero_y_ultimo.append(lista[5])
 
 # COMPLETAR - FIN
 
@@ -80,6 +101,12 @@ lista = ["ho", 3.1416, "la", 81, 6, 42]
 
 # COMPLETAR - INICIO
 
+lista_primeros=[]
+
+lista_primeros.append(lista[0])
+lista_primeros.append(lista[1])
+lista_primeros.append(lista[2])
+
 # COMPLETAR - FIN
 
 assert lista_primeros == ["ho", 3.1416, "la"]
@@ -93,6 +120,8 @@ Restricción: Utilizar indexado múltiple
 lista = ["ho", 3.1416, "la", 81, 6, 42]
 
 # COMPLETAR - INICIO
+
+lista_primeros=lista[0:3]
 
 # COMPLETAR - FIN
 
@@ -109,6 +138,11 @@ lista = ["ho", "la", 81, 6, 42, "como", "estas?"]
 
 # COMPLETAR - INICIO
 
+lista_primeros_y_ultimos=[]
+
+lista_primeros_y_ultimos.extend(lista[0:2])
+lista_primeros_y_ultimos.extend(lista[5:7])
+
 # COMPLETAR - FIN
 
 assert lista_primeros_y_ultimos == ["ho", "la", "como", "estas?"]
@@ -124,6 +158,8 @@ lista_02 = [5, 6]
 
 # COMPLETAR - INICIO
 
+lista_concatenada=lista_01+lista_02
+
 # COMPLETAR - FIN
 
 assert lista_concatenada == [0, 1, 2, 3, 5, 6]
@@ -137,6 +173,8 @@ Restricción: Utiliar el operador *
 lista_01 = [0, 1, 0, 1, 0, 1]
 
 # COMPLETAR - INICIO
+
+lista_duplicada=lista_01*3
 
 # COMPLETAR - FIN
 
@@ -153,6 +191,8 @@ lista = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1.0, 1, 0, 1, 0, 1]
 
 # COMPLETAR - INICIO
 
+variable_booleana=elemento in lista
+
 # COMPLETAR - FIN
 
 assert variable_booleana
@@ -167,6 +207,8 @@ lista_01 = [1, 2, 3, 4.5, 6, 7]
 lista_02 = [1, 3, 2, 4, 5, 6, 7]
 
 # COMPLETAR - INICIO
+
+son_iguales=lista_01==lista_02
 
 # COMPLETAR - FIN
 
@@ -184,6 +226,8 @@ notas = [False, False, False, False, False, False, False, False, False]
 
 # COMPLETAR - INICIO
 
+no_tiene_examenes_aprobados= any(notas)==False
+
 # COMPLETAR - FIN
 
 assert no_tiene_examenes_aprobados
@@ -199,6 +243,8 @@ Restricción: Utilizar el método all
 notas = [True, True, False, True, True, True, True, True, True, True, True, True]
 
 # COMPLETAR - INICIO
+
+tiene_todo_aprobado=all(notas)
 
 # COMPLETAR - FIN
 
