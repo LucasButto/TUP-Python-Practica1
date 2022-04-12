5df643fe9d74add40dae06fa7fc4991efd3af3bf
LucasButto <lbutto40@gmail.com>
2022-04-12 15:46:13 -0300

    Ejercicio 9 resuelto

--- exercises/exercise9.py
+++ exercises/exercise9.py
@@ -6,6 +6,9 @@ Inicializar un conjunto vacío y agregarle los valores de las siguiente variable
 Restricción: Utilizar el metodo add
 """
 
+from typing import Set
+
+
 numero_1 = 1
 numero_2 = 2
 numero_3 = 3
@@ -13,7 +16,9 @@ numero_3 = 3
 # COMPLETAR - INICIO
 
 conjunto_1=set()
-conjunto_1.add(numero_1,numero_2,numero_3)
+conjunto_1.add(numero_1)
+conjunto_1.add(numero_2)
+conjunto_1.add(numero_3)
 
 # COMPLETAR - FIN
 
@@ -32,6 +37,11 @@ edad = "35"
 
 # COMPLETAR - INICIO
 
+conjunto_2={"5","6","7"}
+conjunto_2.add(nombre)
+conjunto_2.add(domicilio)
+conjunto_2.add(edad)
+
 # COMPLETAR - FIN
 
 assert conjunto_2 == {"35", "Esteban", "7", "6", "Los sauces 3446", "5"}
@@ -46,6 +56,8 @@ conjunto_04 = {12, 4, 10, 21, 78}
 
 # COMPLETAR - INICIO
 
+conjunto_interseccion=conjunto_03&conjunto_04
+
 # COMPLETAR - FIN
 
 assert conjunto_interseccion == {10, 4, 21}
@@ -58,6 +70,8 @@ conjunto_03 = {1, 23, 4, 8, 5, 10, 15, 21}
 conjunto_04 = {12, 4, 10, 21, 78}
 
 # COMPLETAR - INICIO
+
+conjunto_interseccion=conjunto_03.intersection(conjunto_04)
 
 # COMPLETAR - FIN
 
@@ -73,6 +87,8 @@ conjunto_06 = {5, 6, 7, 8}
 
 # COMPLETAR - INICIO
 
+conjunto_union=conjunto_05 | conjunto_06
+
 # COMPLETAR - FIN
 
 assert conjunto_union == {1, 2, 3, 4, 5, 6, 7, 8}
@@ -86,6 +102,8 @@ conjunto_05 = {1, 2, 3, 4}
 conjunto_06 = {5, 6, 7, 8}
 
 # COMPLETAR - INICIO
+
+conjunto_union=conjunto_05.union(conjunto_06)
 
 # COMPLETAR - FIN
 
@@ -101,6 +119,8 @@ conjunto_08 = {2, 4, 6, 8}
 
 # COMPLETAR - INICIO
 
+conjunto_diferencia=conjunto_07-conjunto_08
+
 # COMPLETAR - FIN
 
 assert conjunto_diferencia == {1, 3, 5, 7}
@@ -114,6 +134,8 @@ conjunto_07 = {1, 2, 3, 4, 5, 6, 7, 8}
 conjunto_08 = {2, 4, 6, 8}
 
 # COMPLETAR - INICIO
+
+conjunto_diferencia=conjunto_07.difference(conjunto_08)
 
 # COMPLETAR - FIN
 
@@ -129,6 +151,8 @@ conjunto_10 = {1, 2, 3, 5, 6, 7, 8}
 
 # COMPLETAR - INICIO
 
+conjunto_diferencia_simetrica=conjunto_09^conjunto_10
+
 # COMPLETAR - FIN
 
 assert conjunto_diferencia_simetrica == {4, 9}
@@ -142,6 +166,8 @@ conjunto_09 = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 conjunto_10 = {1, 2, 3, 5, 6, 7, 8}
 
 # COMPLETAR - INICIO
+
+conjunto_diferencia_simetrica=conjunto_09.symmetric_difference(conjunto_10)
 
 # COMPLETAR - FIN
 
